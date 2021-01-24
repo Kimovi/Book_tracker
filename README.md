4f3a46c6960ad97a51f8440ae19f98c9ca8afc35
Bora Kim <stellakkkk@hotmail.com>
2021-01-24 18:57:47 +0000

    Initialized git

---
+++ README.md
@@ -0,0 +1,3 @@
+#Let's FLASK it!
+This is exiting new project using Python and Flask. 
+I will be working on another CRUD project, this time I will add DB as well!
---
+++ app.py
@@ -0,0 +1,5 @@
+print("Hello World")
+
+
+# You are using pip version 19.0.3, however version 20.3.3 is available.
+# You should consider upgrading via the 'pip install --upgrade pip' command.
