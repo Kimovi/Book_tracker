46b2efa7efeca07c1a15d1861fe9aa079d9215c5
Bora Kim <stellakkkk@hotmail.com>
2021-02-04 17:28:54 +0000

    Initialized project

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-print("Hello World")
+from application import app
+from flask import url_for
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+
 
 
 # You are using pip version 19.0.3, however version 20.3.3 is available.
