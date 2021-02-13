230176649aceed30f3d66c59eeda78324868afa5
Bora Kim <stellakkkk@hotmail.com>
2021-02-13 22:43:42 +0000

    Working on Update page

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://bora:password@localhost:3306/Book"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/dummmm1.db"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://bora:password@localhost:3306/Book"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/dummmm1.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
